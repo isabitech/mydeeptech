aae8948d0bb8573d03c05fbb4b5104e53b162701
damilola <61382051+Baouldguyd@users.noreply.github.com>
2024-11-27 17:10:15 +0100

    userFixBuild

--- src/components/ProtectedRoute/index.tsx
+++ src/components/ProtectedRoute/index.tsx
@@ -1,27 +1,27 @@
-// import useAuth from "@/hooks/ui/useAuth";
-import { Spin } from "antd";
-import { ReactNode } from "react";
+// // import useAuth from "@/hooks/ui/useAuth";
+// import { Spin } from "antd";
+// import { ReactNode } from "react";
 
-interface ProtectedRouteProps {
-  children: ReactNode;
-}
+// interface ProtectedRouteProps {
+//   children: ReactNode;
+// }
 
-const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
-  const { isAuthenticated, loading } = useAuth();
+// const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
+//   const { isAuthenticated, loading } = useAuth();
 
-  if (loading) {
-    return (
-      <div className="flex h-screen items-center justify-center">
-        <Spin />
-      </div>
-    ); // or a loading spinner
-  }
+//   if (loading) {
+//     return (
+//       <div className="flex h-screen items-center justify-center">
+//         <Spin />
+//       </div>
+//     ); // or a loading spinner
+//   }
 
-  if (!isAuthenticated) {
-    return null; // or a redirect to the login page
-  }
+//   if (!isAuthenticated) {
+//     return null; // or a redirect to the login page
+//   }
 
-  return <>{children}</>;
-};
+//   return <>{children}</>;
+// };
 
-export default ProtectedRoute;
+// export default ProtectedRoute;
